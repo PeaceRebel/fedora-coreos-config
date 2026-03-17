8da26c2ac3cb546a4003023fb3cb6be291417c9b
Bipin B Narayan <bbnaraya@redhat.com>
2026-03-17 19:01:56 +0530

    tests/networking: look for afterburn config installed by package on fedora
    Afterbun network config is installed via package on fedora (and only it).
    Otherwise use old path

--- tests/kola/networking/default-network-behavior-change/test.sh
+++ tests/kola/networking/default-network-behavior-change/test.sh
@@ -90,6 +90,10 @@ fi
 # Execute nm-initrd-generator against our default kargs (defined by
 # afterburn drop in) to get the generated initrd network config.
 DEFAULT_KARGS_FILE=/usr/lib/dracut/modules.d/35coreos-network/50-afterburn-network-kargs-default.conf
+if [ "$ID" == "fedora" ]; then
+    DEFAULT_KARGS_FILE=/usr/lib/dracut/modules.d/35ignition-network/50-afterburn-network-kargs-default.conf
+fi
+
 source <(grep -o 'AFTERBURN_NETWORK_KARGS_DEFAULT=.*' $DEFAULT_KARGS_FILE)
 tmpdir=$(mktemp -d)
 /usr/libexec/nm-initrd-generator \
